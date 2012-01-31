39095abe5a13be35deaaa76174b8770ff20f0d0b
sdk_john <sdk_john@simplexi.com>
2012-01-31 12:36:39 +0800

    *commit for adding adminPageContents and xml config

--- class/admin/adminPageContents.php
+++ class/admin/adminPageContents.php
@@ -1,1 +1,11 @@
 <?php
+
+require_once('builder/builderInterface.php');
+
+class adminPageContents extends Controller_Admin
+{
+    protected function run($aArgs)
+    {
+        $this->view(__CLASS__);
+    }
+}
