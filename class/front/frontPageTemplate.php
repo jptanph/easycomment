42fb213f05bc6a896c030bb54a8ce016c928fa4d
sdk_john <sdk_john@simplexi.com>
2012-02-09 17:34:02 +0800

    * last commit for 20120209. * update css for front.

--- class/front/frontPageTemplate.php
+++ class/front/frontPageTemplate.php
@@ -21,22 +21,28 @@ class frontPageTemplate extends Controller_Front
         $sHtml .= "	    <div class='{$this->_sPrefix}contents'>\n";
         $sHtml .= " 		<ul class='{$this->_sPrefix}comments'>\n";
 
-        for($i = 1;  $i < 20 ; $i++)
+        for($i = 1;  $i < 10 ; $i++)
         {
             $sHtml .= "            <li>";
-            $sHtml .= "                <div class='{$this->_sPrefix}author'><span class='{$this->_sPrefix}author_name'>joihn Adrian tan</span><span class='{$this->_sPrefix}date'>2012-02-02 01:00</span></div>";
-            $sHtml .= "                <p class='{$this->_sPrefix}delete_icon'><a href='#'></a></p>";
-            $sHtml .= "                <p class='{$this->_sPrefix}text'>text</p>";
+            $sHtml .= "                <div class='{$this->_sPrefix}author'><span class='{$this->_sPrefix}author_name'>joihn Adrian tan</span><span class='{$this->_sPrefix}date'>2012/08/02 17:56:53</span></div>";
+            $sHtml .= "                <p class='{$this->_sPrefix}delete_icon'><a href='#none'></a></p>";
+            $sHtml .= "                <p class='{$this->_sPrefix}text'>teasddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddasdddddddddddddddddddddddddddddxt</p>";
             $sHtml .= "                <div class='{$this->_sPrefix}delete_comment'>\n";
-            $sHtml .= "delete";
+            $sHtml .= "                    <label>Password : </label><input type='password' /> <p class='expandable_btn' style='border-bottom:none;display:visible;' id='{$sPrefix}send'><a href='#none' onclick='frontPageEasycomment.execSaveComment();'><span>Delete</span></a></p>";
             $sHtml .= "                </div>\n";
             $sHtml .= "            </li>\n";
         }
 
         $sHtml .= "         </ul>\n";
         $sHtml .= "	    </div>\n";
-        $sHtml .= "	    <div>\n";
-        $sHtml .= "		    form here\n";
+        $sHtml .= "	    <div class='{$this->_sPrefix}add_comment'>\n";
+        $sHtml .= "		   <h3>Add Comment</h3>\n";
+        $sHtml .= "		       <p><label>Name : </label><input type='text'></p>\n";
+        $sHtml .= "		       <p><label>Comment : </label><textarea></textarea></p>\n";
+        $sHtml .= "		       <p><label>Password : </label><input type='password'></p>\n";
+        $sHtml .= "		       <p><label>Captcha : </label><span>SASDASDASDASDASD</span></p>\n";
+        $sHtml .= "		       <p><label>Enter Captcha : </label><input type='text'></p>\n";
+
         $sHtml .= "	    </div>\n";
         $sHtml .= "</div>\n";
         $this->importCss(__CLASS__);
